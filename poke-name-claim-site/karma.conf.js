317ac91a92086a5f1a3a2b7a9ed8d63cf44dc858
Kenneth Chen <kc5by@virginia.edu>
2022-06-25 19:08:13 -0400

    renamed website title + associated folders and project names

--- poke-name-claim-site/karma.conf.js
+++ poke-name-claim-site/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nuzlocke-name-claim-site'),
+      dir: require('path').join(__dirname, './coverage/poke-name-claim-site'),
       subdir: '.',
       reporters: [
         { type: 'html' },
